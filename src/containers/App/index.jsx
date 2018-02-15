a6b77ddb1c2012f960e38d438b398ab9a8f6417f
Kim K <koomen@protonmail.com>
2018-02-15 10:53:12 +0100

    removed import;

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -3,7 +3,6 @@ import { Route, Link, Switch } from 'react-router-dom';
 
 import Home from '../Home';
 import NotFound from '../NotFound';
-import './style.css';
 
 class App extends Component {
   render() {
